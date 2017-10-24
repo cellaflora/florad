550865904121f7802fbf77077b9b6937d7f3e3df
Jacob McDorman <jacob@cellaflora.com>
2017-10-24 16:59:55 -0400

    wip: move up to project

--- demo/userapi.js
+++ demo/userapi.js
@@ -1,68 +1,76 @@
 const path = require('path');
 const debug = require('debug')('userapi');
 const AWS = require('aws-sdk');
-
 const Project = require('../project');
 const Gateway = require('../gateway');
 const Lambda = require('../Lambda');
 
-
 const project = new Project({ 
+	name: 'GatewayTest',
 	projectDirectory: path.resolve(__dirname, '..'),
 	buildDirectory: path.resolve(__dirname, 'build'),
 	aws: { profile: 'cellaflora' },
 });
+const { gateway } = project;
 
-const lambda = new Lambda(project, {
+
+project.defineLambda({
 	name: 'getusers',
 	path: path.resolve(__dirname, 'getusers'),
 	runtime: 'nodejs6.10',
 	role: 'arn:aws:iam::339734559946:role/execute_lambda',
 });
 
+gateway.get('/users', gateway.lambda('getusers'));
 
-debug('build and deploy lambdas');
-lambda.build()
-	.then(() => lambda.deploy())
-	.then(deployed => {
-
-		const gateway = new Gateway('GatewayTest', new Date().toJSON());
-
-		gateway.get('/users', (req, res) => {
-
-			req.when('application/json').accepts({ template: '#/templates/mock' });
-			req.integrates({
-				type: gateway.constants.Lambda,
-				uri: `arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${deployed.version.arn}/invocations`
-			});
-			res.when('default').responds({
-				status: 200,
-				model: '#/definitions/Empty',
-			});
-
-		});
-
-		return gateway.run()
-
-	})
-	.then(schema => {
-
-		const apigatewaySDK = new AWS.APIGateway({
-			apiVersion: '2015-07-09',
-			credentials: new AWS.SharedIniFileCredentials({profile: project.aws.profile}),
-			region: project.aws.region,
-		});
-
-		const definition = {
-			body: new Buffer(JSON.stringify(schema)),
-			failOnWarnings: true,
-			restApiId: 'klkeg3s40h',
-			mode: 'overwrite',
-		};
-
-		// importRestAPI
-		return apigatewaySDK.putRestApi(definition).promise()
-
-	})
+project.build()
+	.then(() => project.deploy())
 	.then(console.log)
 	.catch(console.error);
+
+
+
+// debug('build and deploy lambdas');
+// project.build()
+// 	.then(() => project.deploy())
+// 	.then(deployed => {
+
+// 		project.gateway.get('/users', (req, res) => {
+
+// 			req.when('application/json').accepts({ template: '#/templates/mock' });
+// 			req.integrates({
+// 				type: gateway.constants.Lambda,
+// 				uri: `arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${deployed.version.arn}/invocations`
+// 			});
+// 			res.when('default').responds({
+// 				status: 200,
+// 				model: '#/definitions/Empty',
+// 			});
+
+// 		});
+
+// 		return project.gateway.run()
+
+// 	})
+// 	.then(schema => {
+
+// 		const apigatewaySDK = new AWS.APIGateway({
+// 			apiVersion: '2015-07-09',
+// 			credentials: new AWS.SharedIniFileCredentials({profile: project.aws.profile}),
+// 			region: project.aws.region,
+// 		});
+
+// 		const definition = {
+// 			body: new Buffer(JSON.stringify(schema)),
+// 			failOnWarnings: true,
+// 			restApiId: 'klkeg3s40h',
+// 			mode: 'overwrite',
+// 		};
+
+// 		// importRestAPI
+// 		// return apigatewaySDK.putRestApi(definition).promise()
+// 		return schema;
+
+// 	})
+// 	.then(console.log)
+// 	.catch(console.error);
--- project.js
+++ project.js
@@ -1,12 +1,21 @@
 const path = require('path');
 const debug = require('debug')('project');
 const NPMModules = require('./npm-modules');
+const Lambda = require('./lambda');
+const Gateway = require('./gateway');
+const Queue = require('promise-queue');
 
 
 
 class LambdaProject {
 
-	constructor ({projectDirectory, buildDirectory, aws = {}}) {
+	constructor ({
+		name,
+		projectDirectory,
+		buildDirectory,
+		aws = {},
+		concurency = 4})
+	{
 
 		Object.assign(this, {projectDirectory, buildDirectory});
 		this.moduleDirectory = path.resolve(this.projectDirectory, 'node_modules');
@@ -19,12 +28,49 @@ class LambdaProject {
 		this.repository = this.package.repository;
 		this.author = this.package.author;
 		this.license = this.package.license;
+		this.lambdas = [];
 
 		this.aws = Object.assign({
 			profile: 'default',
 			region: 'us-east-1',
 			deployBucket: null,
 		}, aws);
+
+		this.apiName = `${name}-${this.package.version}`;
+
+		const lambdaExtension = gateway => {
+			gateway.lambda = name => {
+				return (req, res)  => {
+
+					const lambda = this.lambdas.filter(l => l.name === name)[0];
+					if (!lambda) {
+						throw new Error(`Lambda middleware could not find ${name}`);
+					}
+
+					const arn = lambda.version.arn;
+					if (!arn) {
+						throw new Error(`Lambda middleware could not find ${name} arn. ` +
+							`Be sure to deploy lambda first!`);
+					}
+
+					req.when('application/json').accepts({ template: '#/templates/mock' });
+					req.integrates({
+						type: gateway.constants.Lambda,
+						uri: `arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/${arn}/lambda-arn/invocations`
+					});
+					res.when('default').responds({
+						status: 200,
+						model: '#/definitions/Empty',
+					});
+
+				}
+			};
+		};
+
+		this.gateway = new Gateway(this.apiName, new Date().toJSON(), [lambdaExtension]);
+
+		this._buildQueue = new Queue(concurency);
+		this._deployQueue = new Queue(concurency);
 
 	}
 
@@ -42,6 +88,43 @@ class LambdaProject {
 
 	npmWhichBin (command) {
 		return path.resolve(path.resolve(this.muduleBinDirectory, command));
+	}
+
+
+	defineLambda (definition) {
+		this.lambdas.push(new Lambda(this, definition));
+	}
+
+
+	build () {
+
+		debug('BUILD LAMBDAS');
+		const lambdas = this.lambdas;
+		const queue = this._buildQueue;
+		const doLambdaBuild = Promise.all(lambdas.map(l => queue.add(() => l.build())));
+
+		return doLambdaBuild
+			.then(() => {
+				debug('BUILD GATEWAY');
+				return this.gateway.run()
+			})
+			.then(() => this);
+
+	}
+
+
+	deploy () {
+
+		debug('DEPLOY LAMBDAS');
+		const lambdas = this.lambdas;
+		const queue = this._buildQueue;
+		const doLambdaDeploy = Promise.all(lambdas.map(l => queue.add(() => l.deploy())));
+
+		return doLambdaDeploy
+			.then(() => {
+				debug('FINISHED');
+				return this;
+			});
 	}
 
 }
